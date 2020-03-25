2ae95a2311b8c4d19ccb74def5d802ac8a60ed97
苏苏_WSL <suningyo@gmail.com>
2020-03-25 10:19:23 +0800

    统一底部

--- src/components/AppBottomTabbar.vue
+++ src/components/AppBottomTabbar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="miao">
-    <!-- miao qwq -->
-    <!-- WSL_SSH_TEST -->
+    <van-divider
+      :style="{padding: '0 100px', color: 'rgb(174, 174, 174)', borderColor: 'rgb(174, 174, 174)' }"
+    >我是有底线的</van-divider>
   </div>
 </template>
 
