45618d8cae9a77c21e327600f3f6d4119d828146
苏苏_WSL <suningyo@gmail.com>
2020-06-23 08:45:10 +0800

    支持登录

---
+++ src/views/Login.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="login">
+    <van-nav-bar :border="false" @click-left="onNavBarLeftClick">
+      <van-icon name="arrow-left" slot="left" />
+    </van-nav-bar>
+    <div class="login-2a642626758deefdc2989a73aae823d3">
+      <van-cell-group>
+        <van-field
+          v-show="loginInfo.type == 0"
+          v-model="loginInfo.data.name"
+          left-icon="user-o"
+          placeholder="请输入用户名 (仅支持中英文数字)"
+        />
+        <van-field
+          v-model="loginInfo.data.email"
+          left-icon="/img/app-icons/email.png"
+          placeholder="请输入邮箱"
+        />
+        <van-field
+          v-model="loginInfo.data.password"
+          type="password"
+          left-icon="/img/app-icons/password.png"
+          placeholder="请输入密码"
+        />
+        <van-field
+          v-show="loginInfo.type == 2"
+          v-model="loginInfo.data.newPassword"
+          type="password"
+          left-icon="/img/app-icons/password.png"
+          placeholder="请输入新密码"
+        />
+        <van-field
+          v-show="loginInfo.type == 0"
+          v-model="loginInfo.data.qq"
+          left-icon="/img/app-icons/qq.png"
+          placeholder="请输入QQ"
+        />
+      </van-cell-group>
+    </div>
+
+    <div v-show="loginInfo.type == 0" class="login-61046f2f5eefe3dc179e52af32241062">
+      <span class="login-e4c9479b11955648dad558fe717a4eb2">注:用户名和QQ将用于展示在BP界面以及修改密码,请不要瞎写</span>
+    </div>
+
+    <div class="login-6920626369b1f05844f5e3d6f93b5f6e">
+      <van-button
+        round
+        size="small"
+        color="linear-gradient(to right, #4bb0ff, #6149f6)"
+        @click="onLoginClick"
+        class="my-0162f4b7b2dbdf6aff3a25de02e49a8b"
+      >{{ loginInfo.text }}</van-button>
+    </div>
+
+    <div class="login-f01ae8c7f2d058ec6be00db589a32bea login-60ae25445ac62d5ec51c776826888d9f">
+      <span @click="onRegisterClick">注册</span>
+    </div>
+
+    <div class="login-f01ae8c7f2d058ec6be00db589a32bea login-4ae1ffb5939d592986bed21d0913562d">
+      <span @click="onUpdateClick">修改密码</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-6920626369b1f05844f5e3d6f93b5f6e,
+.login-61046f2f5eefe3dc179e52af32241062 {
+  margin-top: 25px;
+}
+
+.login-61046f2f5eefe3dc179e52af32241062 {
+  text-align: center;
+}
+
+.login-e4c9479b11955648dad558fe717a4eb2 {
+  font-size: 12px;
+  color: red;
+}
+
+.login-f01ae8c7f2d058ec6be00db589a32bea {
+  position: absolute;
+  bottom: 50px;
+}
+
+.login-60ae25445ac62d5ec51c776826888d9f {
+  color: #1989fa;
+  left: 25px;
+}
+
+.login-4ae1ffb5939d592986bed21d0913562d {
+  color: red;
+  right: 25px;
+}
+</style>
+
+<script>
+import md5 from "js-md5";
+
+export default {
+  data() {
+    return {
+      loginInfo: {
+        type: 1,
+        text: "登录",
+        data: {
+          name: null,
+          email: null,
+          password: null,
+          newPassword: null,
+          qq: null,
+          openId: null,
+          accessToken: null
+        }
+      }
+    };
+  },
+  methods: {
+    onRegisterClick: function() {
+      this.loginInfo.type = 0;
+      this.loginInfo.text = "注册";
+    },
+    onNavBarLeftClick: function() {
+      if (this.loginInfo.type == 1) {
+        this.$router.go(-1);
+      } else {
+        this.loginInfo.type = 1;
+        this.loginInfo.text = "登录";
+      }
+    },
+    onLoginClick: function() {
+      let type = this.loginInfo.type;
+      let data = this.loginInfo.data;
+
+      let postData = {
+        name: data.name,
+        email: data.email,
+        password: md5(data.password),
+        newPassword: data.newPassword ? md5(data.newPassword) : null,
+        qq: data.qq,
+        openId: data.openId,
+        accessToken: data.accessToken
+      };
+
+      this.axios
+        .post(
+          this.apiList.pvp.loginWebAccount + "&aid=" + type,
+          this.$qs.stringify(postData)
+        )
+        .then(ret => {
+          let data = ret.data.data;
+          let status = ret.data.status;
+
+          if (status.code == 200) {
+            let msg;
+            if (type == 0) msg = "注册成功";
+            if (type == 1) msg = "登录成功";
+            if (type == 2) msg = "修改成功";
+
+            this.$message.success(msg);
+
+            this.$cookie.set("openId", data.openId, { expires: "1Y" });
+            this.$cookie.set("accessToken", data.accessToken, {
+              expires: "1Y"
+            });
+
+            setTimeout(() => {
+              location.href = "/my?from=303e895c08b9161f632e872ed9844a15";
+            }, 500);
+          } else {
+            this.$message.error(status.msg);
+          }
+        });
+    },
+    onUpdateClick: function() {
+      this.loginInfo.type = 2;
+      this.loginInfo.text = "修改密码";
+    }
+  }
+};
+</script>
--- src/views/My.vue
+++ src/views/My.vue
@@ -1,36 +1,63 @@
 <template>
   <div class="my">
-    <van-nav-bar v-if="isLogin" :border="false">
-      <van-icon name="edit" slot="right" />
-    </van-nav-bar>
+    <div v-if="isLogin == false" class="my-3d1d6b29e66d9b4f061e24a2551e2b67">
+      <van-cell-group :border="false" title=" " class="my-058928a73f2a944d621b028eb9addd36">
+        <van-cell template #title>
+          <van-button
+            v-if="isLogin == false"
+            round
+            size="small"
+            color="linear-gradient(to right, #4bb0ff, #6149f6)"
+            @click="$router.push({path: '/login', query: { from: 'b1498f85b6e654f37af3901db4c13cc7' }})"
+            class="my-0162f4b7b2dbdf6aff3a25de02e49a8b"
+          >登录</van-button>
+        </van-cell>
+      </van-cell-group>
+    </div>
 
     <div v-if="isLogin" class="my-cd77c40b62763f6adf5598bce7fceede">
       <img
         width="100"
         height="100"
-        src="https://i.loli.net/2020/06/20/jNdowuY2EIvk4Uf.jpg"
+        v-lazy="loginInfo.img"
         class="my-d5ca322453f2986b752e58b11af83d96"
       />
       <div class="my-f9c7cabc13f359223ebc3ccf9cc104b8">
-        <span class="my-25930e3036f13852cb0b29694bbab611 my-b068931cc450442b63f5b3d276ea4297">苏苏</span>
+        <span
+          class="my-25930e3036f13852cb0b29694bbab611 my-b068931cc450442b63f5b3d276ea4297"
+        >{{ loginInfo.name }}</span>
         <span class="my-25930e3036f13852cb0b29694bbab611 my-293a35164a20c927b0fd61942fbc1cf2">
-          <van-tag round color="red">管理员</van-tag>
+          <van-tag round :color="loginInfo.certification.color">{{ loginInfo.certification.text }}</van-tag>
         </span>
       </div>
     </div>
 
     <div class="my-7dc22b2c6a992f0232345df41303f5ea">
-      <van-cell-group :border="false" title=" " class="my-058928a73f2a944d621b028eb9addd36">
-        <van-cell v-if="isLogin == false" title="登录" is-link @click="isLogin = true" />
-        <van-grid v-if="isLogin" :border="false" :column-num="2">
-          <van-grid-item icon="/img/app-icons/team.png" text="10" />
-          <van-grid-item icon="/img/app-icons/bp_data.png" text="200" to="/game/new" />
-        </van-grid>
-      </van-cell-group>
+      <van-grid v-if="isLogin" :border="false" :column-num="2">
+        <van-grid-item icon="/img/app-icons/team.png" template #text>
+          <div class="my-6e8737d4ac83f11c858de8bde0a6c52a">
+            <span class="my-4646fa4296a7f5dea261e60e00ecd24b">{{ loginInfo.statistics.teamNum }}</span>
+            <span class="my-7a33dbf09bb2e3ed21ecb1adf0cb37b4">支</span>
+          </div>
+        </van-grid-item>
+        <van-grid-item icon="/img/app-icons/bp_data.png" to="/game/new/bp" template #text>
+          <div class="my-6e8737d4ac83f11c858de8bde0a6c52a">
+            <span class="my-4646fa4296a7f5dea261e60e00ecd24b">{{ loginInfo.statistics.labelNum }}</span>
+            <span class="my-7a33dbf09bb2e3ed21ecb1adf0cb37b4">局</span>
+          </div>
+        </van-grid-item>
+      </van-grid>
     </div>
 
     <div class="my-7dc22b2c6a992f0232345df41303f5ea">
       <van-cell-group :border="false" title=" " class="my-058928a73f2a944d621b028eb9addd36">
+        <van-cell
+          icon="friends-o"
+          title="特别感谢"
+          value="互相帮助过的伙伴们"
+          is-link
+          url="https://doc.91m.top"
+        />
         <van-cell icon="question-o" title="常见问题" value="自豪的使用语雀" is-link url="https://doc.91m.top" />
         <van-cell
           icon="comment-o"
@@ -44,17 +71,21 @@
 
     <div v-if="isLogin" class="my-4cf71de630f99f4bf37ea1218fdab416">
       <van-cell-group :border="false" title=" " class="my-058928a73f2a944d621b028eb9addd36">
-        <van-cell
-          v-if="isLogin"
-          title="退出登录"
-          title-class="my-4236a440a662cc8253d7536e5aa17942"
-          @click="isLogin = false"
-        />
+        <van-cell template #title>
+          <van-button
+            v-if="isLogin"
+            round
+            size="small"
+            color="rgb(245, 245, 245)"
+            @click="onLogotClick"
+            class="my-4236a440a662cc8253d7536e5aa17942"
+          >退出登录</van-button>
+        </van-cell>
       </van-cell-group>
     </div>
 
     <div class="my-111fb4d92036323891a140cd49383f86">
-      <van-cell-group :border="false" title=" ">
+      <van-cell-group :border="false">
         <van-cell
           title="沪ICP备16031287号-2"
           url="http://beian.miit.gov.cn"
@@ -70,38 +101,44 @@ div.my-7dc22b2c6a992f0232345df41303f5ea
   div.van-grid
   div.van-grid-item
   div.van-grid-item__content
-  span.van-grid-item__text {
+  span.van-grid-item__text,
+.my-4646fa4296a7f5dea261e60e00ecd24b {
   font-size: 20px;
+}
+
+button.my-4236a440a662cc8253d7536e5aa17942 span.van-button__text {
+  color: red;
 }
 </style>
 
 <style scoped>
+.my-25930e3036f13852cb0b29694bbab611 {
+  margin: 0 3px;
+}
+
+.my-b068931cc450442b63f5b3d276ea4297 {
+  font-size: 10px;
+}
+
+.my-cd77c40b62763f6adf5598bce7fceede {
+  margin-top: 35px;
+  margin-bottom: 10px;
+}
+
 .my-d5ca322453f2986b752e58b11af83d96 {
   border-radius: 100%;
 }
 
 .my-f9c7cabc13f359223ebc3ccf9cc104b8 {
-  margin-top: 10px;
+  margin-top: 20px;
 }
 
-.my-25930e3036f13852cb0b29694bbab611 {
-  margin: 0 3px;
+.my-7a33dbf09bb2e3ed21ecb1adf0cb37b4 {
+  margin-left: 5px;
 }
 
-.my-293a35164a20c927b0fd61942fbc1cf2 {
-  position: absolute;
-  margin-top: -2px;
-}
-
-.my-4cf71de630f99f4bf37ea1218fdab416 {
-  width: 200px;
-  margin: 0 auto;
-}
-
-.my-4236a440a662cc8253d7536e5aa17942 {
-  color: red;
-  line-height: 40px;
-  border-radius: 100px;
+.my-058928a73f2a944d621b028eb9addd36 {
+  margin-top: -20px;
 }
 
 .my-7dc22b2c6a992f0232345df41303f5ea {
@@ -118,10 +155,55 @@ export default {
   name: "My",
   data() {
     return {
-      isLogin: false
+      isLogin: false,
+      loginInfo: {}
     };
   },
-  mounted() {},
-  methods: {}
+  mounted() {
+    this.onLoginCheck();
+  },
+  methods: {
+    onLoginCheck: function() {
+      let openId = this.$cookie.get("openId");
+      let accessToken = this.$cookie.get("accessToken");
+
+      if (!openId || !accessToken) return;
+
+      let postData = {
+        openId: openId,
+        accessToken: accessToken
+      };
+
+      this.axios
+        .post(this.apiList.pvp.getWebAccountInfo, this.$qs.stringify(postData))
+        .then(ret => {
+          let data = ret.data.data;
+          let status = ret.data.status;
+
+          if (status.code == 200) {
+            this.isLogin = true;
+            this.loginInfo = data;
+          } else {
+            this.isLogin = false;
+            this.$message.error(status.msg);
+          }
+        });
+    },
+    onLogotClick: function() {
+      this.$dialog
+        .confirm({
+          title: "是否退出登录？"
+        })
+        .then(() => {
+          // on confirm
+          this.$cookie.delete("openId");
+          this.$cookie.delete("accessToken");
+          this.isLogin = false;
+        })
+        .catch(() => {
+          // on cancel
+        });
+    }
+  }
 };
 </script>
