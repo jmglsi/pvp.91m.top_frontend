370ae429b53f96366f2092ab57bf18f14cee8d12
苏苏_WSL <suningyo@gmail.com>
2020-06-24 15:13:37 +0800

    数据图模块化

---
+++ src/components/Hero/Line.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="hero-line">
+    <div class="hero-965f1a65ae362b02d244345afcbf542e">
+      <ve-line
+        :settings="tableData.settings"
+        :extend="extend"
+        :mark-line="markLine"
+        :mark-point="markPoint"
+        :data="tableData.result"
+        width="99%"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import VeLine from "v-charts/lib/line.common";
+
+import "echarts/lib/component/markLine";
+import "echarts/lib/component/markPoint";
+import "echarts/lib/component/dataZoom";
+
+export default {
+  name: "HeroLine",
+  components: {
+    VeLine
+  },
+  props: {
+    heroId: {
+      type: Number,
+      default: 0
+    },
+    detailed: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    listenChange() {
+      const { heroId, detailed } = this;
+      return { heroId, detailed };
+    }
+  },
+  watch: {
+    listenChange: {
+      immediate: false,
+      handler(newValue, oldValue) {
+        this.getHeroChartsLog(newValue.heroId, Number(newValue.detailed));
+        if (oldValue.heroId != 0) {
+          this.$message.success("切换成功");
+          document.body.scrollTop = document.documentElement.scrollTop = 0;
+        }
+      }
+    }
+  },
+  data() {
+    this.markLine = {
+      data: [
+        {
+          name: "平均线",
+          type: "average"
+        }
+      ]
+    };
+    this.markPoint = {
+      data: [
+        {
+          name: "最大值",
+          type: "max"
+        }
+      ]
+    };
+    this.extend = {
+      xAxis: {
+        axisLabel: {
+          rotate: 45
+        }
+      },
+      dataZoom: [
+        {
+          type: "slider",
+          show: true,
+          start: 80,
+          end: 100
+        }
+      ]
+    };
+    return {
+      tableData: {
+        result: []
+      }
+    };
+  },
+  methods: {
+    getHeroChartsLog: function(heroId, detailed) {
+      this.axios
+        .get(
+          this.apiList.pvp.getHeroChartsLog +
+            "&heroId=" +
+            heroId +
+            "&detailed=" +
+            detailed
+        )
+        .then(ret => {
+          let data = ret.data.data;
+          let chartData = data.result;
+          if (chartData.rows.length != 0) {
+            this.tableData = ret.data.data;
+          }
+        });
+    }
+  }
+};
+</script>
---
+++ src/components/Hero/Radar.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="hero-radar">
+    <div class="hero-b86b97a57d04d7096b93b34cea410cfe">
+      <vue-tags-input
+        v-model="tag.text"
+        v-show="tabsModel == 2"
+        :tags="tag.array"
+        :placeholder="tag.placeholder"
+        @tags-changed="onTagsChanged"
+        @blur="onTagsBlur"
+        class="info-d57ac45256849d9b13e2422d91580fb9"
+      />
+    </div>
+
+    <div class="hero-00c0096b2f59afa06ea70de0e19fff29">
+      <ve-radar
+        legend-position="bottom"
+        :style="style"
+        :height="appDevice ? '650px' : '750px'"
+        :settings="tableData.settings"
+        :data="tableData.result"
+      />
+    </div>
+  </div>
+</template>
+
+<style>
+div.ti-input {
+  border-radius: 5px;
+}
+</style>
+
+<style scoped>
+.info-d57ac45256849d9b13e2422d91580fb9 {
+  text-align: center;
+  width: 85%;
+  max-width: 500px;
+  margin: 0 auto;
+  margin-bottom: 25px;
+}
+</style>
+
+<script>
+import VeRadar from "v-charts/lib/radar.common";
+
+export default {
+  name: "HeroRadar",
+  components: {
+    VeRadar,
+    VueTagsInput: resolve => require(["@johmun/vue-tags-input"], resolve)
+  },
+  props: {
+    tabsModel: {
+      type: Number,
+      default: 0
+    },
+    heroId: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    listenChange() {
+      const { tabsModel, heroId } = this;
+      return { tabsModel, heroId };
+    }
+  },
+  watch: {
+    listenChange: {
+      immediate: true,
+      handler(newValue) {
+        if (newValue.tabsModel == 1) {
+          this.getHeroChartsLogBySimilar(newValue.heroId);
+        } else {
+          this.tag.array = [];
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      tag: {
+        text: "",
+        array: [],
+        placeholder: "输入对比的英雄,点击任意空白位置确认 😄"
+      },
+      style: {
+        marginTop: 0
+      },
+      tableData: {
+        result: []
+      },
+      customize: {
+        old: "",
+        new: ""
+      }
+    };
+  },
+  mounted() {
+    this.appDevice
+      ? (this.style.marginTop = "-150px")
+      : (this.style.marginTop = "-50px");
+  },
+  methods: {
+    getHeroChartsLogBySimilar: function(heroId) {
+      this.axios
+        .get(this.apiList.pvp.getHeroChartsLogBySimilar + "&heroId=" + heroId)
+        .then(ret => {
+          let data = ret.data.data;
+          let chartData = data.result;
+          if (chartData.rows.length != 0) {
+            this.tableData = ret.data.data;
+          }
+        });
+    },
+    getHeroChartsLogByCustomize: function(heroName) {
+      this.axios
+        .get(
+          this.apiList.pvp.getHeroChartsLogByCustomize + "&heroName=" + heroName
+        )
+        .then(ret => {
+          let data = ret.data.data;
+          let chartData = data.result;
+          if (chartData.rows.length != 0) {
+            this.tableData = ret.data.data;
+          }
+        });
+    },
+    getHeroChartsLogByCustomizeFromUrl: function(heroName) {
+      let e, hero, newTags;
+      e = heroName.split(",");
+      for (let i = 0; i < e.length; i++) {
+        hero = e[i];
+        newTags = newTags + "," + hero;
+        this.tag.array.push({ text: decodeURI(hero) });
+      }
+
+      this.getHeroChartsLogByCustomize(heroName);
+    },
+    onTagsChanged: function(e) {
+      if (e.length == 0) {
+        this.chartSettings = {};
+        this.chartData = [];
+        this.customize = "";
+      } else {
+        let hero, newTags;
+        for (let i = 0; i < e.length; i++) {
+          hero = e[i].text;
+          newTags = newTags + "," + hero;
+        }
+        this.customize.new = newTags.replace("undefined,", "");
+      }
+    },
+    onTagsBlur: function() {
+      let oldCustomize = this.customize.old;
+      let newCustomize = this.customize.new;
+
+      if (oldCustomize != newCustomize) {
+        this.getHeroChartsLogByCustomize(newCustomize);
+        this.customize.old = newCustomize;
+      }
+    }
+  }
+};
+</script>
