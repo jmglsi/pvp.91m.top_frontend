a9815ffd85be87639223a8f711ea4d6e927b1b53
苏苏_WSL <suningyo@gmail.com>
2020-07-30 18:18:32 +0800

    新增 国际化翻译

--- src/assets/import/vxe-table.js
+++ src/assets/import/vxe-table.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import XEUtils from 'xe-utils'
 
 import {
     VXETable,
@@ -6,8 +7,11 @@ import {
     Column,
     Header,
     Grid,
-    Table
+    Table,
+    Toolbar,
+    Button
 } from 'vxe-table'
+import zhCNLocat from 'vxe-table/lib/locale/lang/zh-CN'
 
 VXETable.setup({
     table: {
@@ -17,9 +21,10 @@ VXETable.setup({
         showOverflow: null,
         highlightCurrentRow: true,
         highlightHoverRow: true
-    }
+    },
+    i18n: (key, value) => XEUtils.get(zhCNLocat, key, value)
 });
 
-Vue.use(Column).use(Icon).use(Header).use(Grid).use(Table)
+Vue.use(Column).use(Icon).use(Header).use(Grid).use(Table).use(Toolbar).use(Button)
 
 Vue.prototype.$XModal = VXETable.modal
