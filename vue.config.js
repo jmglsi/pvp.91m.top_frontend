d3822ca71ad37369a06af30da9ed18d1bcb0131d
苏苏_WSL <suningyo@gmail.com>
2020-12-16 08:37:46 +0800

    规范命名

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    productionSourceMap: false,
-    css: {
-        loaderOptions: {
-            less: {
-                javascriptEnabled: true
-            }
-        }
-    },
     pwa: {
         name: '苏苏的荣耀助手 - 全局BP模拟器',
         short_name: '荣耀助手',
@@ -14,5 +6,13 @@ module.exports = {
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'default'
-    }
+    },
+    css: {
+        loaderOptions: {
+            less: {
+                javascriptEnabled: true
+            }
+        }
+    },
+    productionSourceMap: false
 }
