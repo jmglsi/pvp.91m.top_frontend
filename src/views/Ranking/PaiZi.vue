5c0fef686bbc88b58602e9aa579f51be2b16ed65
苏苏_WSL <suningyo@gmail.com>
2021-02-23 12:19:08 +0800

    新增 金牌查询

---
+++ src/views/Ranking/PaiZi.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="ranking-pz app-pz">
+    <div class="ranking-3ede7e85e7bd91a85bce2a134d18fb18">
+      <vxe-grid
+        ref="refPaiZi"
+        :loading="tableData.loading"
+        :data="tableData.result.rows"
+        :height="clientHeight"
+        empty-text="点击上方筛选"
+        @cell-click="onCellClick"
+      >
+        <vxe-table-column
+          title="英雄"
+          field="name"
+          fixed="left"
+          width="75"
+          sortable
+        >
+          <template v-slot="{ row }">
+            <div
+              :class="
+                isSmallMobile == 1
+                  ? 'app-1de7efdd403ec02d55f5c1d9557a2fc4'
+                  : null
+              "
+            >
+              <img
+                v-lazy="row.img"
+                width="50"
+                height="50"
+                class="ranking-b798abe6e1b1318ee36b0dcb3fb9e4d3"
+              />
+            </div>
+          </template>
+        </vxe-table-column>
+
+        <vxe-table-column title="#" type="seq" width="75" />
+
+        <vxe-table-column
+          title="战力"
+          field="fightPowerValue"
+          width="75"
+          sortable
+        />
+
+        <vxe-table-column
+          title="更新时间"
+          field="updateTime"
+          :width="listWidth"
+          sortable
+        />
+      </vxe-grid>
+    </div>
+
+    <div class="ranking-84226baebc9c90dd5bba99237b39725a">
+      <van-action-sheet
+        v-model="showInfo.fightPowerMenu"
+        :title="tableDataRow.name + ' 如何操作'"
+        safe-area-inset-bottom
+      >
+        <HeroFightPower
+          v-if="showInfo.fightPowerMenu"
+          :heroId="tableDataRow.id"
+          :fightPowerType="did"
+        />
+      </van-action-sheet>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaiZi",
+  components: {
+    HeroFightPower: () => import("@/components/Hero/FightPower.vue"),
+  },
+  data() {
+    return {
+      tableData: {
+        color: {},
+        column: [],
+        columns: [],
+        loading: false,
+        result: {
+          rows: [],
+        },
+      },
+      tableDataRow: {
+        id: 0,
+        name: "加载中",
+      },
+      listWidth: 0,
+      clientHeight: 0,
+      showInfo: {
+        fightPowerMenu: false,
+      },
+    };
+  },
+  props: {
+    isSmallMobile: {
+      type: Number,
+      default: 0,
+    },
+    bid: {
+      type: Number,
+      default: 0,
+    },
+    cid: {
+      type: Number,
+      default: 0,
+    },
+    did: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    listenChange() {
+      const { isSmallMobile, bid, cid, did } = this;
+      return { isSmallMobile, bid, cid, did };
+    },
+  },
+  watch: {
+    listenChange: {
+      immediate: true,
+      handler(newValue) {
+        this.getRanking(4, newValue.bid, newValue.cid, newValue.did);
+
+        this.$appPush({
+          query: {
+            type: 4,
+            bid: newValue.bid,
+            cid: newValue.cid,
+            did: newValue.did,
+            refresh: 0,
+          },
+        });
+      },
+    },
+  },
+  created() {
+    this.clientHeight = this.$appInitTableHeight(10);
+    this.listWidth = this.$appInitTableWidth(350);
+  },
+  mounted() {},
+  methods: {
+    getRanking: function (aid = 4, bid = 0, cid = 0, did = 0) {
+      if (bid == 0 || cid == 0 || did == 0) return;
+
+      this.tableData.loading = true;
+
+      this.$axios
+        .post(
+          this.$appApi.pvp.getRanking +
+            "&aid=" +
+            aid +
+            "&bid=" +
+            bid +
+            "&cid=" +
+            cid +
+            "&did=" +
+            did
+        )
+        .then((res) => {
+          let data = res.data.data,
+            status = res.data.status;
+
+          if (status.code == 200) {
+            this.tableData = data;
+            this.tableData.loading = false;
+
+            this.$message.info(this.$appMsg.info[1019]);
+          } else {
+            this.$message.error(status.msg);
+          }
+        });
+    },
+    onCellClick: function ({ row }) {
+      this.tableDataRow = row;
+
+      this.showInfo.fightPowerMenu = true;
+    },
+  },
+};
+</script>
