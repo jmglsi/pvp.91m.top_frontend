5caa5e4a3ee1ddd3b46ef9042ba0ea3c33e40041
jmglsi_Mac <suningyo@gmail.com>
2022-09-15 09:15:37 +0800

    修复已知问题

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ],
-  plugins: [
-    ['import', { libraryName: 'vxe-table', style: true }, 'vxe-table'],
-    ['import', { libraryName: 'vant', libraryDirectory: 'es', style: (e) => `${e}/style/less` }, 'vant'],
-    ['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: 'css' }, 'ant-design-vue']
-  ]
+    presets: [
+        '@vue/app'
+    ],
+    plugins: [
+        ['import', { libraryName: 'vxe-table', style: true }, 'vxe-table'],
+        ['import', { libraryName: 'vant', libraryDirectory: 'es', style: (e) => `${e}/style/less` }, 'vant'],
+        ['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: 'css' }, 'ant-design-vue']
+    ]
 }
--- src/assets/import/antd-icons.js
+++ src/assets/import/antd-icons.js
@@ -1,28 +1,35 @@
 /**
- * 
+ *
  * 图标按需导入
- * 
+ *
  * @author 西瓜弟弟
  * @link https://juejin.cn/post/6844904068230414343
  */
 
-export { default as CloseCircleFill } from '@ant-design/icons/lib/fill/CloseCircleFill';
+export { default as CloseCircleFill }
+from '@ant-design/icons/lib/fill/CloseCircleFill';
 //error
 
-export { default as InfoCircleFill } from '@ant-design/icons/lib/fill/InfoCircleFill';
+export { default as InfoCircleFill }
+from '@ant-design/icons/lib/fill/InfoCircleFill';
 //info
 
-export { default as CheckCircleFill } from '@ant-design/icons/lib/fill/CheckCircleFill';
+export { default as CheckCircleFill }
+from '@ant-design/icons/lib/fill/CheckCircleFill';
 //success
 
-export { default as ExclamationCircleFill } from '@ant-design/icons/lib/fill/ExclamationCircleFill';
+export { default as ExclamationCircleFill }
+from '@ant-design/icons/lib/fill/ExclamationCircleFill';
 //warning
 
-export { default as LoadingOutline } from '@ant-design/icons/lib/outline/LoadingOutline';
+export { default as LoadingOutline }
+from '@ant-design/icons/lib/outline/LoadingOutline';
 //loading
 
-export { default as SearchOutline } from '@ant-design/icons/lib/outline/SearchOutline';
+export { default as SearchOutline }
+from '@ant-design/icons/lib/outline/SearchOutline';
 //search
 
-export { default as CloseOutline } from '@ant-design/icons/lib/outline/CloseOutline';
+export { default as CloseOutline }
+from '@ant-design/icons/lib/outline/CloseOutline';
 //close
